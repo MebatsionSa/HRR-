f5ad7084c600c017b6b97760165e959565850762
MebatsionSa <mebatsionsahle@gmail.com>
2020-01-19 17:34:28 +0300

    Fourth Commit

--- VerificationController.php
+++ VerificationController.php
@@ -37,5 +37,5 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
-    }
+    }//Third Commit
 }
